9e67c4201739875b746bf63e3dd5313c25617123
Raphael LeBlanc <35542943+RaphaelLeBlanc@users.noreply.github.com>
2018-04-25 13:23:25 -0400

    Add files via upload

---
+++ Final/upinder.js
@@ -0,0 +1,14 @@
+
+function show1()
+{
+	console.log("C");
+	document.getElementById("divu").style.display = "none";
+	
+}
+
+function show2()
+{
+	console.log("C");
+	document.getElementById("divu").style.display = "block";
+	
+}
